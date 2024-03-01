3df54e973f0c000760ee7780b090845b8cbee9af
berkanlafci <berkan.lafci@uzh.ch>
2024-03-01 15:44:10 +0100

    camera

---
+++ mvps/camera/manageCamera.py
@@ -0,0 +1,9 @@
+#-----
+# Description   : Camera object properties and functions
+# Author        : Berkan Lafci
+# E-mail        : lafciberkan@gmail.com
+#-----
+
+# import libraries
+import os
+import sys
