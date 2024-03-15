530da3d92111a7ed5bafcac287e12d223f9ffc25
berkanlafci <berkan.lafci@uzh.ch>
2024-03-15 10:11:35 +0100

    reconstruction bash script

---
+++ recon.sh
@@ -0,0 +1,7 @@
+#-----
+# Description   : Reconstruction script
+# Author        : Berkan Lafci
+# E-mail        : lafciberkan@gmail.com
+#-----
+
+python -B mvps/examples/exampleRecon.py --dataDir data/ --objectName object
