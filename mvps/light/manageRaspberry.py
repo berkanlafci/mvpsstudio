5f84614216ab5b54c2fdc03c6f1a58f9ec86c4fc
berkanlafci <berkan.lafci@uzh.ch>
2024-04-24 17:19:35 +0200

    raspberry pi manager

---
+++ mvps/light/manageRaspberry.py
@@ -0,0 +1,50 @@
+#-----
+# Description   : Connect to raspberry pi and run light control codes
+# Author        : Berkan Lafci
+# E-mail        : lafciberkan@gmail.com
+#-----
+
+# import libraries
+import subprocess
+
+class RaspberryManager():
+
+    def __init__(self):
+        """
+        Object to manage raspberry pi connection and light control
+        """
+
+        # raspberry pi parameters
+        self.raspberryName  = 'light' # name of the raspberry pi machine
+        self.userName       = 'person' # change with your username
+        self.scriptName     = 'sparseFour.py' # name of the light control script
+    
+    #-------------------------------#
+    #---------- functions ----------#
+    #-------------------------------#
+    
+    #-------------------------------#
+    # copy script to raspberry pi
+        
+    def copyScript(self):
+
+        # copy light pattern script to raspberry pi
+        subprocess.run(['scp', 'mvps/light/'+ self.scriptName, self.userName + '@' + self.raspberryName +':~/'])
+
+    #-------------------------------#
+    # create ssh connection to raspberry pi
+
+    def openSshConnection(self):
+
+        # create ssh connection process
+        self.sshProcess = subprocess.Popen(['ssh', '-tt', self.raspberryName], 
+                                           stdin=subprocess.PIPE, stdout = subprocess.PIPE,
+                                           universal_newlines=True, bufsize=0)
+
+    #-------------------------------#
+    # run light control script on raspberry pi
+         
+    def runScript(self):
+
+        # run copied light script
+        self.sshProcess.stdin.write('sudo /home/'+ self.userName +'/.light/bin/python3 ~/' + self.scriptName +'\n')
